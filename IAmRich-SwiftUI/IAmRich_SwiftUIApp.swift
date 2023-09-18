fe07bba08cf80db93eaa702dfc000a4e3857a9ff
Mark <mark.arias7@gmail.com>
2023-09-17 22:23:51 -0500

    Initial Commit

---
+++ IAmRich-SwiftUI/IAmRich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRich_SwiftUIApp.swift
+//  IAmRich-SwiftUI
+//
+//  Created by Mark Arias on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct IAmRich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
